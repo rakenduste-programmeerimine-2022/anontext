80e3a35024bc3cb70abe25e111638e8b4ce06aba
Hendrik Oja <90192364+hendrikoja@users.noreply.github.com>
2023-01-11 20:40:40 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.f7stczs.mongodb.net/?retryWrites=true&w=majority`
+
 mongoose
   .connect(uri)
   .then(() => console.log('Database connection established'))
